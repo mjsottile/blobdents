f895e0f2402afaebf0e19890e7651ed4548aa92e
Matthew Sottile <mjsottile@gmail.com>
2014-12-23 00:25:04 -0800

    edits

---
+++ test_images/flowers/blobber.m
@@ -0,0 +1,24 @@
+d=dir('*.jpg');
+for i=1:length(d)
+    im{i}=imread(d(i).name);
+end
+
+g{1} = rgb2gray(im{1}) > 120;
+g{2} = im{2}(:,:,3) > 120;
+g{3} = im{3}(:,:,1) > 140;
+g{4} = im{4}(:,:,1) > 150;
+g{4}(255:260,1575:1700) = 0;
+g{4}(260:300,1632:1640) = 0;
+g{5} = rgb2gray(im{5}) > 120;
+
+for i=1:5
+    CC = bwconncomp(g{i});
+    BW{i} = zeros(size(g{i}));
+    numPixels = cellfun(@numel,CC.PixelIdxList);
+    [biggest,idx] = max(numPixels);
+    BW{i}(CC.PixelIdxList{idx}) = 1;
+    B = bwboundaries(BW{i});
+    bd{i} = B{1};    
+    subplot(2,3,i);plot(bd{i}(:,2),bd{i}(:,1));
+end
+
